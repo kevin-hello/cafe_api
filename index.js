1061f0a8e52184b6f5d5487f3eed0f0f90aabac8
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-21 13:30:30 -0700

    fixing /cafes endpoint

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Cafes.find().then((cafes) => {
-      res.status(200).json(cafes);
+      res.status(201).json(cafes);
     });
   }
 );
